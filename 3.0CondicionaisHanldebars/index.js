2282baf753985370e2a18abc5bb00490eac3dd64
alexPietro <alexpieetro@gmail.com>
2024-06-10 10:35:10 -0300

    git commit -m

--- 3.0CondicionaisHanldebars/index.js
+++ 3.0CondicionaisHanldebars/index.js
@@ -9,11 +9,16 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     const user = {
-        firstName: "Alex",
-        lastName: "Cerqueira",
+        firstName: 'Alex',
+        lastName: 'Cerqueira',
     }
-    res.render("home", {user: user});
+    const autentificator = false
+    res.render('home', {user: user, autentificator});
 });
+
+app.get("/dashboard", (req, res) =>{
+    res.render('paginaInternaSistema')
+})
 
 app.listen(3000, () => {
     console.log("App rodando na porta localhost:3000");
